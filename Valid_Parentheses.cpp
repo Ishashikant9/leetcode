f25753619f9c93848ada1ddc499b2566aed6c24a
Ishashikant9 <shashikant77078@gmail.com>
2024-06-12 19:46:35 -0700

    Valid_parentheses

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; // create an empty stack to store opening brackets
+        for (char c : s) { // loop through each character in the string
+            if (c == '(' || c == '{' || c == '[') { // if the character is an opening bracket
+                st.push(c); // push it onto the stack
+            } else { // if the character is a closing bracket
+                if (st.empty() || // if the stack is empty or 
+                    (c == ')' && st.top() != '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; // the string is not valid, so return false
+                }
+                st.pop(); // otherwise, pop the opening bracket from the stack
+            }
+        }
+        return st.empty(); // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+                           // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "{[()]}"; // you can change this string to test different cases
+    if (solution.isValid(s)) {
+        cout << "The string is valid." << endl;
+    } else {
+        cout << "The string is not valid." << endl;
+    }
+    return 0;
+}
