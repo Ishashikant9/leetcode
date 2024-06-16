1b1a77715e22719202905f8efed8e3f2e0195261
Ishashikant9 <shashikant77078@gmail.com>
2024-06-15 21:13:08 -0700

    Remove_Element

---
+++ Remove_Element.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {3, 2, 2, 3, 4, 2, 5};
+    int val = 2;
+    int newLength = solution.removeElement(nums, val);
+
+    cout << "New length: " << newLength << endl;
+    cout << "Modified array: ";
+    for (int i = 0; i < newLength; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
