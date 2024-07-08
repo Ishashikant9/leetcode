15557b0b5c8be00ea6f01d5dc7d0f752c0c6b0d5
Ishashikant9 <shashikant77078@gmail.com>
2024-07-07 21:37:02 -0700

    Isomorphic_String

---
+++ Isomorphic_String.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> indexS(200, 0); // Stores index of characters in string s
+        vector<int> indexT(200, 0); // Stores index of characters in string t
+        
+        int len = s.length(); // Get the length of both strings
+        
+        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+            return false;
+        }
+        
+        for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                return false; // If different, strings are not isomorphic
+            }
+            
+            indexS[s[i]] = i + 1; // updating position of current character
+            indexT[t[i]] = i + 1;
+        }
+        
+        return true; // If the loop completes without returning false, strings are isomorphic
+    }
+};
+
+int main() {
+    Solution solution;
+    string s1, s2;
+
+    cout << "Enter first string: ";
+    cin >> s1;
+
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    bool result = solution.isIsomorphic(s1, s2);
+
+    if (result) {
+        cout << "The strings are isomorphic." << endl;
+    } else {
+        cout << "The strings are not isomorphic." << endl;
+    }
+
+    return 0;
+}
