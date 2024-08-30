bd073ac39c3da0128d7b025778c4bd2cfc5c5917
Ishashikant9 <shashikant77078@gmail.com>
2024-08-30 07:20:39 +0530

    Hamming Distance

---
+++ Hamming_Distance.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int bit_x = 0;
+        int bit_y = 0;
+        int count = 0;
+        while (x != 0 || y != 0) {
+            bit_x = x & 1;
+            bit_y = y & 1;
+            if (bit_x != bit_y) {
+                count++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    int x = 1, y = 4;
+    cout << sol.hammingDistance(x, y) << endl;
+    return 0;
+}
