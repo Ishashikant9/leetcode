402caaa2570590701d2fc4d20cfc1c384225617a
Ishashikant9 <shashikant77078@gmail.com>
2024-09-01 18:17:41 +0530

    Repeated Substring Pattern

---
+++ RepeatedSubstringPattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                string substring = s.substr(0, i);
+                string repeated = "";
+                for (int j = 0; j < n / i; ++j) {
+                    repeated += substring;
+                }
+                if (repeated == s) return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "abab";  // example value
+    cout << sol.repeatedSubstringPattern(s) << endl;
+    return 0;
+}
