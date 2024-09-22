b931e6a967b1f63c49a8c5810d116900ea462a2e
Ishashikant9 <shashikant77078@gmail.com>
2024-09-22 12:11:56 +0530

    Find First and Last Element Position in Sorted Array

---
+++ findFirstandLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result = {-1, -1};
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        result[0] = left;
+        result[1] = right;
+        return result;        
+    }
+
+    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int idx = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                idx = mid;
+                if (isSearchingLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        
+        return idx;
+    }    
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {5, 7, 7, 8, 8, 10};  // Predefined input for testing
+    int target = 8;
+
+    vector<int> result = sol.searchRange(nums, target);
+
+    // Output the result
+    cout << "First and last positions of target " << target << " are: [";
+    cout << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
+}
