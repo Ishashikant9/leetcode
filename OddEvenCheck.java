5f7ada1f9bb2a1cd423faf43119a680246413714
ShashiKant <shashikant77078@gmail.com>
2025-02-14 21:42:46 +0530

    check number odd or even

---
+++ OddEvenCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddEvenCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+        
+        scanner.close();
+    }
+}
