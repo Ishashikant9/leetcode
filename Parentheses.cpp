708c9c89262f25271c6724172bc848e44fe63609
Shashi Kant <shashikant77078@gmail.com>
2025-03-21 21:02:24 +0530

    Create Parentheses.cpp

---
+++ Parentheses.cpp
@@ -0,0 +1,21 @@
+ class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0;  
+        int close = 0; 
+
+    
+        for (char c : s) {
+            if (c == '(') {
+                open++; 
+                } else if (c == ')' && open > 0) {
+                open--; 
+            } else {
+                close++; 
+            }
+        }
+        
+        
+        return open + close;
+    }
+};
